e01475f52bb80649370f1320851d6b69df11404c
WilliamNylund <william.nylund@gmail.com>
2023-02-24 12:18:04 +0200

    cleanup etc

--- whiteboard/src/Board.tsx
+++ whiteboard/src/Board.tsx
@@ -25,43 +25,38 @@ const Board = (props: Iprops) => {
     : ['mousemove', 'mousedown', 'mouseup'];
 
   const fetchCanvas = async () => {
-    axios
-      .get(BASE_URL + '/canvas')
-      .then((res) => {
-        let image = new Image();
-        let canvas = document.querySelector('#board');
-        let ctx = canvas.getContext('2d');
-        image.onload = () => {
-          ctx.drawImage(image, 0, 0);
-          isDrawing = false;
-        };
-        image.src = res.data;
-      });
+    axios.get(BASE_URL + '/canvas').then((res) => {
+      let image = new Image();
+      let canvas = document.querySelector('#board');
+      let ctx = canvas.getContext('2d');
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0);
+        isDrawing = false;
+      };
+      image.src = res.data;
+    });
   };
 
   useEffect(() => {
     fetchCanvas();
     drawOnCanvas();
 
-    socket.on(
-      'canvas-data',
-      (data) => {
-        let interval = setInterval(() => {
-          if (isDrawing) return;
-          isDrawing = true;
-          clearInterval(interval);
-          let image = new Image();
-          let canvas = document.querySelector('#board');
-          let ctx = canvas.getContext('2d');
-          image.onload = () => {
-            ctx.drawImage(image, 0, 0, canvas?.clientWidth, canvas?.clientHeight ); //, canvas?.clientWidth, canvas?.clientHeight 
-            isDrawing = false;
-          };          
-          image.src = data;
-        }, 200);
-      }
-    );
-    
+    socket.on('canvas-data', (data) => {
+      let interval = setInterval(() => {
+        if (isDrawing) return;
+        isDrawing = true;
+        clearInterval(interval);
+        let image = new Image();
+        let canvas = document.querySelector('#board');
+        let ctx = canvas.getContext('2d');
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0, canvas?.clientWidth, canvas?.clientHeight);
+          isDrawing = false;
+        };
+        image.src = data;
+      }, 200);
+    });
+
     return () => {
       socket.off('canvas-data');
     };
@@ -84,70 +79,53 @@ const Board = (props: Iprops) => {
     canvas.height = canvas?.clientHeight;
     let mouse = { x: undefined, y: undefined };
     let last_mouse = { x: 0, y: 0 };
-    
 
-    window.addEventListener(
-      'resize',
-      () => {
-        let inMemCanvas = document.createElement('canvas');
-        let inMemCtx = inMemCanvas.getContext('2d');
-        let newWidth = canvas?.clientWidth;
-        let newHeight = canvas?.clientHeight;
-        ctx.strokeStyle = props.color;
-        ctx.lineWidth = props.size;
-        if (canvas.width != newWidth || canvas.height != newHeight){
-          inMemCanvas.width = canvas.width;
-          inMemCanvas.height = canvas.height;
-          inMemCtx.drawImage(canvas, 0, 0);
-          canvas.width = newWidth
-          canvas.height = newHeight
-          ctx.drawImage(inMemCanvas, 0, 0, newWidth, newHeight);
-        }
+    window.addEventListener('resize', () => {
+      let inMemCanvas = document.createElement('canvas');
+      let inMemCtx = inMemCanvas.getContext('2d');
+      let newWidth = canvas?.clientWidth;
+      let newHeight = canvas?.clientHeight;
+      ctx.strokeStyle = props.color;
+      ctx.lineWidth = props.size;
+      if (canvas.width != newWidth || canvas.height != newHeight) {
+        inMemCanvas.width = canvas.width;
+        inMemCanvas.height = canvas.height;
+        inMemCtx.drawImage(canvas, 0, 0);
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        ctx.drawImage(inMemCanvas, 0, 0, newWidth, newHeight);
       }
-    );
-  
-    // Capturing mouse movement
-    canvas.addEventListener(
-      typeOfInteraction[0],
-      function (e) {
-        e.preventDefault();
-        last_mouse.x = mouse.x;
-        last_mouse.y = mouse.y;
+    });
 
-        if (props.isMobile) {
-          mouse.x = e.changedTouches[0].pageX - this.offsetLeft;
-          mouse.y = e.changedTouches[0].pageY - this.offsetTop;
-        } else {
-          mouse.x = e.pageX
-          mouse.y = e.pageY - this.offsetTop
-        }
-      },
-      false
-    );
+    // Capturing mouse movement
+    canvas.addEventListener(typeOfInteraction[0], function (e) {
+      e.preventDefault();
+      last_mouse.x = mouse.x;
+      last_mouse.y = mouse.y;
+      if (props.isMobile) {
+        mouse.x = e.changedTouches[0].pageX - this.offsetLeft;
+        mouse.y = e.changedTouches[0].pageY - this.offsetTop;
+      } else {
+        mouse.x = e.pageX;
+        mouse.y = e.pageY - this.offsetTop;
+      }
+    });
 
     ctx.lineWidth = props.size;
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.strokeStyle = props.color;
 
-    canvas.addEventListener(
-      typeOfInteraction[1],
-      (e) => {
-        e.preventDefault();
-        canvas.addEventListener(typeOfInteraction[0], onPaint, false);
-      },
-      false
-    );
+    canvas.addEventListener(typeOfInteraction[1], (e) => {
+      e.preventDefault();
+      canvas.addEventListener(typeOfInteraction[0], onPaint);
+    });
 
-    canvas.addEventListener(
-      typeOfInteraction[2],
-      (e) => {
-        e.preventDefault();
-        mouse = { x: undefined, y: undefined };
-        canvas.removeEventListener(typeOfInteraction[0], onPaint, false);
-      },
-      false
-    );
+    canvas.addEventListener(typeOfInteraction[2], (e) => {
+      e.preventDefault();
+      mouse = { x: undefined, y: undefined };
+      canvas.removeEventListener(typeOfInteraction[0], onPaint);
+    });
 
     const onPaint = () => {
       ctx.beginPath();
@@ -159,24 +137,21 @@ const Board = (props: Iprops) => {
       if (timeout != undefined) clearTimeout(timeout);
       timeout = setTimeout(() => {
         let base64ImageData = canvas.toDataURL('image/png');
-        socket.emit(
-          'canvas-data',
-          base64ImageData
-        );
+        socket.emit('canvas-data', base64ImageData);
       }, 1000);
     };
   };
 
   return (
     <div className="sketch" id="sketch">
-      <div className='centered'>
-        <div className='error-main'>
-          <h2 className='header-404'>404</h2>
-
+      <div className="centered">
+        <div className="error-main">
+          <h2 className="header-404">404</h2>
         </div>
-        <p className='text-404'>Oops, the page you're looking for does not exist.</p>
-        <p className='sub'>Greet other lost souls with a drawing!</p>
-        
+        <p className="text-404">
+          Oops, the page you're looking for does not exist.
+        </p>
+        <p className="sub">Greet other lost souls with a drawing!</p>
       </div>
       <canvas className="board" id="board"></canvas>
     </div>
